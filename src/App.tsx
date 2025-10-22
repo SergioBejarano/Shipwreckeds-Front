88ce5e2496036d1c639cdb4d0079205f719b1acd
Daniel <daniel.ruge-g@mail.escuelaing.edu.co>
2025-10-22 12:29:40 -0500

    limpieza

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-import React, { useState } from "react";
-import Portada from "./components/Portada";
 
 function App() {
-  const [logueado, setLogueado] = useState(false);
-
-  const handleIniciarSesion = () => {
-    // Aquí luego rediriges al lobby o pantalla de login real
-    setLogueado(true);
-  };
-
   return (
     <div className="App">
-      {!logueado ? (
-        <Portada onIniciarSesion={handleIniciarSesion} />
-      ) : (
-        <div>
-          {/* Aquí tu componente de login/lobby */}
-          Bienvenido al juego
-        </div>
-      )}
     </div>
   );
 }
